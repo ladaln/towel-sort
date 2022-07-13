1e02ed4c3aefd50daa853859f2783d0bc84e902a
Lada <100614341+ladaln@users.noreply.github.com>
2022-07-13 08:49:41 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,5 +4,5 @@
 module.exports = function towelSort (matrix) {
    matrix.reduce((acc, cur, i) => {
     cur.sort((a, b) => !(i & 1) ? a - b : b - a).map(e => acc.push(e));
-    return matrix;
+    return acc;
   }, [])
